263c7e405ed41b5de819f27f6a8bf10ce0fd2113
carloshenriqueok <ifcarloshenrique@gmail.com>
2026-04-16 17:35:40 -0300

    FEAT: configs iniciais para a pagina de delegação [REFS: #5]

---
+++ src/views/DelagationView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AppHeaderMob from '@/components/layout/mobile/AppHeaderMob.vue';
+</script>
+
+<template>
+<AppHeaderMob title="Delegação"/>
+</template>
+
+<style scoped></style>
