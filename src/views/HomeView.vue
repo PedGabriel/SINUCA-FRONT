7ab5ce3a5252bb54e6e1e98cd528bf820081b766
carloshenriqueok <ifcarloshenrique@gmail.com>
2026-05-15 13:27:25 -0300

    FEAT: criando página de "bem-vindo" para a seleção do tipo de usuário

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter()
 </script>
 
 <template>
+    <button @click="router.push('/p')">playground</button>
+    <button @click="router.push('/delegacao')">delegacao</button>
 </template>
 
 <style scoped>
--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,27 +1,50 @@
 <script setup>
 import LogoAzul from '/logo-azul.svg'
+
+const options = [
+    {
+        id: 1, 
+        icon: "mdi mdi-crown-outline", 
+        title: "Organizador", 
+        desc: "Gerencie o evento e controle as atividades.",
+        iconColor: "color: #849324",
+        border: "border: 2px solid #849324",
+    },
+    {
+        id: 2, 
+        icon: "mdi mdi-account-outline", 
+        title: "Discente", 
+        desc: "Represente seu país e participe das tarefas.",
+        iconColor: "color: #FFB30F",
+        border: "border: 2px solid #FFB30F",
+    },
+    {
+        id: 3, 
+        icon: "mdi mdi-star-outline", 
+        title: "Avaliador", 
+        desc: "Avalie as delegações e seu desempenho.",
+        iconColor: "color: #FD151B",
+        border: "border: 2px solid #FD151B",
+    },
+]
+
 </script>
 
 <template>
     <section class="container">
         <img :src="LogoAzul" alt="logo sinuca" class="logo"/>
         <h1>BEM-VINDO</h1>
-        <h3>Selecione como deseja acessar o sistema</h3>
-        <ul class="option-list">
-            <li class="organizer">
-                <span class="mdi mdi-crown-outline"></span>
-                <h3>Organizador</h3>
-                <p>Gerencie o evento e controle as atividades.</p>
-            </li>
-            <li class="student">
-                <span class="mdi mdi-account-outline"></span>
-                <h3>Discente</h3>
-                <p>Represente seu país e participe das tarefas.</p>
-            </li>
-            <li class="appraiser">
-                <span class="mdi mdi-star-outline"></span>
-                <h3>Avaliador</h3>
-                <p>Avalie as delegações e seu desempenho.</p>
+        <p class="subtitle">Selecione como deseja acessar o sistema</p>
+        <ul class="options-list">
+            <li 
+                v-for="opt in options"
+                :key="opt.id"
+                class="option-item"
+                :style="opt.border"
+            >   
+                <span :class="opt.icon" :style="opt.iconColor"></span>
+                <h3 style="font-weight: 500;">{{ opt.title }}</h3>
+                <p>{{ opt.desc }}</p>
             </li>
         </ul>
     </section>
@@ -33,19 +56,49 @@ import LogoAzul from '/logo-azul.svg'
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    text-align: center;
     height: 100vh;
-    background-image: url('/fundo-continente.svg');
+    background-image: url('/static/fundo-continente.svg');
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
 }
 
-.container p {
-    color: #;
+.subtitle{
+    width: 67%;
+    color: #969696;
+    margin: 0.5rem auto;
 }
 
-.option-list li{
-
+.options-list {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    gap: 2rem;
+    margin-top: 2rem;
 }
 
+.option-item {
+    background-color: white;
+    padding: 2rem 0.5rem;
+    border-radius: 10px;
+    box-shadow:  0 -1px 10px rgba(0, 0, 0, 0.6);
+    transition: all .2s;
+    width: 60%;
+    margin: 0 auto;
+
+}
+.option-item p {
+    color: #969696;
+    margin-top: 0.5rem;
+}
+
+.option-item span {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+}
+
+.option-item:active {
+    transform: scale(0.98);
+}
 </style>
