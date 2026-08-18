719a3e829433760a2c9d44dabd074b546e49b703
PedGabriel <pedrogabrielgoncalves91@gmail.com>
2026-08-18 09:32:18 -0300

    CHORE: Estilização do modal do item de cronograma

--- src/components/delegation/modais/ScheduleModal.vue
+++ src/components/delegation/modais/ScheduleModal.vue
@@ -2,7 +2,7 @@
 import { useScheduleStore } from '@/stores/scheduleStore'
 import { useCountryStore } from '@/stores/countryStore'
 import { useLinkStore } from '@/stores/linkStore'
-import { onMounted, ref, computed } from 'vue'
+import { onMounted, ref, computed, onUnmounted } from 'vue'
 
 
 const props = defineProps({
@@ -36,6 +36,7 @@ const countrys = ref([])
 const links = ref([])
 
 onMounted(async () => {
+    //document.body.style.overflow = 'hidden'
     scheduleStore.getSchedules()
     await scheduleStore.getSchedule(props.scheduleId)
 
@@ -61,50 +62,66 @@ const nameCategory = computed(() => {
             return ''
     }
 })
+
+const emits = defineEmits(['close']);
+
+/*onUnmounted(() => {
+  document.body.style.overflow = ''
+})*/
+
 </script>
 
 <template>
-    <section v-if="scheduleStore.schedule.category == 1"></section>
-    <section v-else-if="scheduleStore.schedule.category == 2">
+    <div class="background" @click="emits('close')"></div>
+    <section v-if="scheduleStore.schedule.category == 1" class="modal"></section>
+    <section v-else-if="scheduleStore.schedule.category == 2" class="modal">
         <div class="header">
-            <h1>{{ nameCategory }}</h1>
-            <button>
-                <span class="mdi mdi-close"></span>
-            </button>
+            <div class="title">
+            <h2>Debate</h2>
+            
+            <span class="mdi mdi-close"
+            style="color: #969696; font-size: 2rem"          @click="emits('close')"
+            ></span>
+            
+            </div>
             <div class="countrys">
                 <div>
                     <img :src="countrys[0].flag.url" alt="" />
-                </div>
-                <h5>
+                    <h5>
                     {{ countrys[0].name }}
-                </h5>
+                    </h5>
+                </div>
                 <span> VS </span>
                 <div>
                     <img :src="countrys[1].flag.url" alt="" />
-                </div>
-                <h5>
+                    <h5>
                     {{ countrys[1].name }}
-                </h5>
+                    </h5>
+                </div>
             </div>
         </div>
         <div class="topic">
             <h4>Tema do debate</h4>
-            <h2>
+            <h3>
                 {{ scheduleStore.schedule.title }}
-            </h2>
+            </h3>
         </div>
         <div>
-            <ul>
+            <ul class="schedules-infos">
                 <li>
+                    <span class="mdi mdi-calendar-month-outline"></span>
                     {{ formatDate(scheduleStore.schedule.startDate) }}
                 </li>
                 <li>
+                    <span class="mdi mdi-map-marker-outline"></span>
                     {{ scheduleStore.schedule.location }}
                 </li>
                 <li>
+                    <span class="mdi mdi-clock-outline"></span>
                     {{ formatTime(scheduleStore.schedule.startDate) }}
                 </li>
                 <li>
+                    <span class="mdi mdi-clock-outline"></span>
                     {{ formatTime(scheduleStore.schedule.endDate) }}
                 </li>
             </ul>
@@ -122,5 +139,66 @@ const nameCategory = computed(() => {
             </div>
         </div>
     </section>
-    <section v-else-if="scheduleStore.schedule.category == 3"></section>
+    <section v-else-if="scheduleStore.schedule.category == 3" class="modal"></section>
 </template>
+
+<style scoped>
+/*
+.background {
+  position: fixed;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  backdrop-filter: blur(2px);
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3));
+  height: 100vh;
+  width: 100%;
+}
+.modal{
+    z-index: 20;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: #ffffff;
+}*/
+
+.header {
+    
+}
+.title {
+    display: flex;
+    justify-content: space-between;
+}
+.countrys {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-weight: 500;
+}
+img {
+    width: 5rem;
+    height: auto;
+    border-radius: 10%;
+}
+.topic {
+    margin-top: 3vw;
+    border: 2px solid #b6b6b6;
+    padding: 1rem;
+    border-radius: 10px;
+}
+.topic h4 {
+    color: #01295F; 
+    
+}
+.topic h3 {
+    font-weight: 700;
+    font-size: 1.1rem;
+    margin-top: 0.5rem;
+}
+
+.schedules-infos{
+    display: flex;
+}
+
+</style>
